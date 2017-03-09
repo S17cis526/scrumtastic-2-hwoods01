c2dd6d05c9f8051059f0de73ca71fdc01f9a2d05
hayden <hayden@hayzak.com>
2017-03-09 14:16:42 -0600

    Changes done in class

---
+++ public/app.js
@@ -0,0 +1,10 @@
+var xhr = new XMLHTTPRequest();
+
+xhr.open('GET', '/projects/');
+xhr.send(null);
+
+xhr.onreadstatechange = function() {
+  var done = 4;
+  var OK = 200;
+  if (xhr.readState ==)
+}
